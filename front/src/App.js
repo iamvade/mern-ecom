d58faaf6bbbeedef2688a2216aeebc0eb70ed69d
iamvade <lkhagvadorj0510@gmail.com>
2020-10-28 22:23:04 +0800

    displayed products

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,19 @@
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
-    <h1>Hello World</h1>
-  );
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
