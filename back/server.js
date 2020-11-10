ab329628a9fd17d0e6765456cd4c409ca8fdb761
iamvade <lkhagvadorj0510@gmail.com>
2020-11-10 13:37:46 +0800

    added error handler middlewares

--- back/server.js
+++ back/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import colors from "colors";
 
 config();
@@ -9,7 +10,8 @@ connectDB();
 const app = express();
 
 app.use("/api/products", productRoutes);
-
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.nextTick.PORT || 5000;
 
 app.listen(
