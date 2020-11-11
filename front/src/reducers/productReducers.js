8a5d32859d8a684aa6abd3514f56cdc9d81adcd7
iamvade <lkhagvadorj0510@gmail.com>
2020-11-11 15:19:31 +0800

    error, loading components

--- front/src/reducers/productReducers.js
+++ front/src/reducers/productReducers.js
@@ -7,15 +7,11 @@ export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
-      break;
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
-      break;
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
-      break;
     default:
       return state;
-      break;
   }
 };
