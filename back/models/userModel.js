c4d6da3d3dc3797d6fa8d198d54f8f56b7a6d052
iamvade <lkhagvadorj0510@gmail.com>
2020-12-06 10:44:21 +0800

    added encrypting and register function

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -30,5 +30,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Hash & salt passwords before save
+userSchema.pre("save", async function (next) {
+  // If password is not changed - Only when changing password or creating new user
+  if (!this.isModified("password")) {
+    next();
+  }
+  // Generate salt
+  const salt = await bcrypt.genSalt(10);
+  // Replace password with encrypted password
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 export default User;
--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import User from "../models/userModel.js";
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
